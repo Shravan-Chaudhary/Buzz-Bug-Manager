2e4182e4d077244205beecf47c66f17d531b7ef5
Shravan Chaudhary <bitznatch1110@gmail.com>
2023-11-21 03:43:06 +0530

    add login logout button

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,37 +1,67 @@
-'use client'
-import MaxWidthWrapper from '@/components/MaxWidthWrapper'
-import Image from 'next/image'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import logo from '@/public/bug.svg'
+"use client"
+import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import logo from "@/public/bug.svg"
+import { useSession } from "next-auth/react"
+import { buttonVariants } from "@/components/ui/button"
 
 const Navbar = () => {
   const currentPath = usePathname()
+  const { status, data: session } = useSession()
   const links = [
-    { label: 'Home', href: '/' },
-    { label: 'Issues', href: '/issues' }
+    { label: "Home", href: "/" },
+    { label: "Issues", href: "/issues" },
   ]
 
   return (
-    <nav className="sticky inset-x-0 top-0 z-40 mb-5 h-14 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all ">
-      <MaxWidthWrapper>
-        <div className="flex h-14 items-center justify-between border-b border-zinc-200">
-          <Link href={'/'} className="z-40 font-semibold md:text-xl">
+    <nav className="sticky inset-x-0 top-0 z-40 mb-5 h-14 w-full border-b border-gray-200 bg-white/30 backdrop-blur-lg transition-all ">
+      <MaxWidthWrapper className="flex h-14 items-center justify-between border-b border-zinc-200">
+        {/* Logo and Links Container */}
+        <div className={"flex gap-10 sm:gap-14 md:gap-20"}>
+          <Link href={"/"} className="z-40 font-semibold md:text-xl">
             <Image src={logo} alt="logo" width={40} height={40} />
           </Link>
-          <ul className="flex items-center space-x-6">
+          <ul className="flex items-center gap-6 md:gap-10">
             {links.map((link) => (
               <Link
                 key={link.href}
                 href={link.href}
                 className={`${
-                  currentPath === link.href ? 'text-zinc-900' : 'text-zinc-500'
+                  currentPath === link.href ? "text-zinc-900" : "text-zinc-500"
                 } z-40 font-semibold transition-colors hover:text-zinc-800 md:text-lg`}
               >
                 <span>{link.label}</span>
               </Link>
             ))}
           </ul>
+        </div>
+
+        {/* SignIn SignOut Button Container */}
+        <div>
+          {status === "unauthenticated" && (
+            <Link
+              href={"/api/auth/signin"}
+              className={buttonVariants({
+                size: "lg",
+                variant: "ghost",
+              })}
+            >
+              Sign In
+            </Link>
+          )}
+          {status === "authenticated" && (
+            <Link
+              href={"/api/auth/signout"}
+              className={buttonVariants({
+                size: "lg",
+                variant: "ghost",
+              })}
+            >
+              Log Out
+            </Link>
+          )}
         </div>
       </MaxWidthWrapper>
     </nav>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,39 @@
-import React from 'react'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import Navbar from './Navbar'
-import { cn } from '@/lib/utils'
-import { ThemeProvider } from '@/components/theme-provider'
-import { Toaster } from '@/components/ui/toaster'
+import React from "react"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
+import Navbar from "./Navbar"
+import { cn } from "@/lib/utils"
+import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toaster"
+import AuthProvider from "@/app/auth/Provider"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: 'Bug Manager',
-  description: 'A tool to manage bugs in your software projects.',
+  title: "Bug Manager",
+  description: "A tool to manage bugs in your software projects.",
   icons: {
-    icon: './favicon.ico'
-  }
+    icon: "./favicon.ico",
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen font-sans antialiased bg-zinc-100', inter.className)}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="light"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <Navbar />
-          <main>{children}</main>
-          <Toaster />
-        </ThemeProvider>
+      <body className={cn("min-h-screen font-sans antialiased bg-zinc-100", inter.className)}>
+        <AuthProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="light"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Navbar />
+            <main>{children}</main>
+            <Toaster />
+          </ThemeProvider>
+        </AuthProvider>
       </body>
     </html>
   )

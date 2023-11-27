52523c668dd59e0c8edbdd7b7e0c3dcabb705e68
Shravan Chaudhary <bitznatch1110@gmail.com>
2023-11-27 13:30:37 +0530

    add middleware to endpoints

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,7 @@
+import { authOptions } from "@/lib/auth"
 import { issueSchema } from "@/lib/validations/issue-schema"
 import prisma from "@/prisma/client"
+import { getServerSession } from "next-auth"
 import { NextRequest, NextResponse } from "next/server"
 
 interface Props {
@@ -10,6 +12,11 @@ interface Props {
 
 // Update Issue
 export async function PATCH(request: NextRequest, { params }: Props) {
+  const session = await getServerSession(authOptions)
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+  }
+
   // validate request body
   const requestBody = await request.json()
   const validatedBody = issueSchema.safeParse(requestBody)
@@ -45,6 +52,11 @@ export async function PATCH(request: NextRequest, { params }: Props) {
 
 // Delete Issue
 export async function DELETE(request: NextRequest, { params }: Props) {
+  const session = await getServerSession(authOptions)
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
+  }
+
   // find issue by id
   const issue = await prisma.issue.findUnique({
     where: {
